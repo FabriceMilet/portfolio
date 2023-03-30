d9219028c7c693aa76dada5654d68cb8515e9d08
FabriceMilet <milet.fab@gmail.com>
2023-03-30 16:40:10 +0200

    mise en place du cube et gestion des lumières et de l'ombre

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,13 @@
 import './Home.scss'
 import { FaArrowAltCircleDown } from "react-icons/fa";
-import { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber'
-import * as THREE from 'three'
+import { useRef } from 'react';
+import { Canvas } from '@react-three/fiber'
 import Cube from './Cube/Cube';
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Shadow, Plane } from '@react-three/drei'
 
 export default function Home({ className, targetContact, setContactHover, setheaderScrolled }) {
   const cameraRef = useRef()
- 
+
   const handleClickContact = (event) => {
     event.preventDefault();
     targetContact.current.scrollIntoView({ behavior: "smooth" });
@@ -20,17 +19,27 @@ export default function Home({ className, targetContact, setContactHover, sethea
 
     <div className={className}>
       <div className="Home-container">
-        <Canvas>
-          <scene>
-          <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-            <Cube />
-            <OrbitControls />
-          </scene>
+        <Canvas shadows camera={{ position: [-5, 2, 10], fov: 60 }}>
+            <ambientLight intensity={0.15}  />
+            <spotLight position={[4, 4, 4]} angle={0.1} penumbra={1}  intensity={1.5} shadow-mapSize={512} />
+            <Plane
+          position={[0, -1, 0]}
+          receiveShadow
+        >
+          <shadowMaterial transparent opacity={0.4} />
+        </Plane>
+            <Shadow
+            width={1024}
+            height={1024}
+            position={[-5, -4, -4]}
+          opacity={1} scale={8} blur={5} far={2}
+          />
+           <directionalLight castShadow position={[10, 10, 0]} intensity={.3} shadow-mapSize={512}/>
+            <Cube position={[0, 1.5, 0]} castShadow receiveShadow  />
+            <OrbitControls />      
         </Canvas>
         <div>
-          <button className="Home-button" onClick={handleClickContact}>Contactez moi</button>
+          <button className="Home-button" onClick={handleClickContact}>Me contacter</button>
           <FaArrowAltCircleDown size={40} />
         </div>
       </div>
