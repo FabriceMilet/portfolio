a3253cc9c53e87db8c038f6c1211e32c0d5ce999
FabriceMilet <milet.fab@gmail.com>
2023-04-05 10:26:56 +0200

    ajout des nouveaux projets

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,5 +1,7 @@
 import './Projects.scss'
 import getBalanced from '../../img/getBalanced.png'
+import portfolio from '../../img/portfolio.png'
+import getBalancedBack from '../../img/getBalancedBack.png'
 import { forwardRef } from 'react';
 
 const Projects = forwardRef(({ className, projectsHover}, targetProjects ) => {
@@ -25,32 +27,32 @@ const Projects = forwardRef(({ className, projectsHover}, targetProjects ) => {
           <img src={getBalanced} alt="Project getBalanced"/>
         </div>
         <section className="Projects-card__info">
-          <h3>Get Balanced</h3>
-          <p className="Projects-card__text">Application qui aide au partage des tâches ménagères dans un foyer </p>
-          <p className="Projects-card__link">Cliquez ici pour en savoir plus</p>
+          <h3>Get Balanced (front)</h3>
+          <p className="Projects-card__text">Partie front d'une application qui aide au partage des tâches ménagères dans un foyer </p>
+          <a href='https://github.com/FabriceMilet/GetBalanced-front' className="Projects-card__link" target="_blank">Cliquez ici pour en savoir plus</a>
         </section>
       </div>
       
     
       <div className="Projects-card">
         <div className="Projects-card__img">
-          <img src={getBalanced} alt="Project getBalanced"/>
+          <img src={getBalancedBack} alt="Project getBalanced"/>
         </div>
         <section className="Projects-card__info">
-          <h3>Get Balanced</h3>
-          <p className="Projects-card__text">Application qui aide au partage des tâches ménagères dans un foyer </p>
-          <p className="Projects-card__link">Cliquez ici pour en savoir plus</p>
+          <h3>Get Balanced (back)</h3>
+          <p className="Projects-card__text">Construction de l'API d'une application qui aide au partage des tâches ménagères dans un foyer </p>
+          <a href='https://github.com/FabriceMilet/GetBalanced-back' className="Projects-card__link" target="_blank">Cliquez ici pour en savoir plus</a>
         </section>
       </div>
      
       <div className="Projects-card">
         <div className="Projects-card__img">
-          <img src={getBalanced} alt="Project getBalanced"/>
+          <img src={portfolio} alt="Project getBalanced"/>
         </div>
         <section className="Projects-card__info">
-          <h3>Get Balanced</h3>
-          <p className="Projects-card__text">Application qui aide au partage des tâches ménagères dans un foyer </p>
-          <p className="Projects-card__link">Cliquez ici pour en savoir plus</p>
+          <h3>Mon portfolio</h3>
+          <p className="Projects-card__text">Construction de ce portfolio, avec REACT, SCSS (et un peu de three.js pour le fun) </p>
+          <a href='https://github.com/FabriceMilet/portfolio' className="Projects-card__link" target="_blank">Cliquez ici pour en savoir plus</a>
         </section>
       </div>
       
